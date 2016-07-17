cb1a5774e9e011040022de9a1696f45cd80c79fa
Irtza <irtzali@gmail.com>
2016-07-17 16:37:24 +0500

    final

---
+++ $tf/7/3fe51507-312e-4806-9d1a-b425f6d58f59.rw
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="awaami.MvcApplication" Language="C#" %>
---
+++ awaami/Controllers/AwaamController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace awaami.Controllers
+{
+    public class AwaamController : Controller
+    {
+        // GET: Awaam
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegisterPost()
+        {
+            return RedirectToAction("login");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult ViewIssues()
+        {
+
+            return View();
+        }
+
+        public ActionResult Authorities()
+        {
+
+            return View();
+        }
+
+    }
+}
